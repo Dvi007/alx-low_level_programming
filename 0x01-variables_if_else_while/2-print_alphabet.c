7e81021bac24c01b848b3711cb211bf0d882a7f8
Dvi007 <daveinfos80@gmail.com>
2023-02-19 20:49:18 -0800

    the putchar white in alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ *
+ * Return: 0(sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
