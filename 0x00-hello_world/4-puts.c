c5bdf53612a5db02c10c86f477dadab9dc7191f5
Dvi007 <daveinfos80@gmail.com>
2023-02-19 19:00:57 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a sentence
+ *
+ * Return: 0 for sucesse
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
