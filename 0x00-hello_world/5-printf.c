85d28e79b8b478e089c1b85b6c914cb22647fd3e
Dvi007 <daveinfos80@gmail.com>
2023-02-19 19:04:51 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a sentense whith printf
+ *
+ * Return: (0) sucess
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
