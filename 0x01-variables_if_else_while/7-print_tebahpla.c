627627c2e4b5d692568a906fab7f6441a0c4eb04
Dvi007 <daveinfos80@gmail.com>
2023-02-21 19:29:12 -0800

    the inverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print inverse the alphabet
+ *
+ * Return: 0 (sucess)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+return (0);
+}
