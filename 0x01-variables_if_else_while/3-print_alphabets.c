4f1680fc4304c8ee9be5b56c33af3a0ce42cc830
Dvi007 <daveinfos80@gmail.com>
2023-02-21 18:58:02 -0800

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ *
+ * Return: char
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
