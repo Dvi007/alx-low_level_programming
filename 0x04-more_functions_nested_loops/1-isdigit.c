3795f0c5d5773f0b81c504519d9165b0a86fa3d3
Dvi007 <daveinfos80@gmail.com>
2023-02-24 09:30:38 -0800

    first step

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - check for a digit
+ * @c: this s the integer
+ * Return: 0 (sucess)
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplication of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 sucess
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
